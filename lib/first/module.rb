b67be53bc473823dc92a74aca798c9472f901add
uharston <uharston@gmail.com>
2020-05-25 13:35:16 -0500

    first commit

---
+++ lib/first/module.rb
@@ -0,0 +1,8 @@
+require "first/module/version"
+
+module First
+  module Module
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/first/module/version.rb
@@ -0,0 +1,5 @@
+module First
+  module Module
+    VERSION = "0.1.0"
+  end
+end
